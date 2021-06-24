60713330e0957a3d56f7ad5cc18f88c05e0ba44e
meteorlxy <meteor.lxy@foxmail.com>
2021-06-25 02:43:21 +0800

    build: bump dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: resolve(__dirname, 'dev'),
