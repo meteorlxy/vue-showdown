92bca84f599382327cbe4bb88ee1181ccb9f0672
meteorlxy <meteor.lxy@foxmail.com>
2022-12-14 13:57:00 +0800

    docs: tweak head title resolver

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,8 +1,11 @@
-import { defineClientConfig } from '@vuepress/client';
+import { defineClientConfig, resolvers } from '@vuepress/client';
 import VueShowdownPlugin from 'vue-showdown';
 
 export default defineClientConfig({
   enhance({ app }) {
+    resolvers.resolvePageHeadTitle = (page, siteLocale) =>
+      [page.title, siteLocale.title].filter((item) => !!item).join(' | ');
+
     app.use(VueShowdownPlugin, {
       options: {
         emoji: true,
